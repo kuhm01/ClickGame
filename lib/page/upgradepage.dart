1542674f485ee0accdfc47515e54650a361dbdd5
kuhm01 <kuhm01@naver.com>
2024-08-01 17:10:23 +0900

    edit upgradepage.dart

--- lib/page/upgradepage.dart
+++ lib/page/upgradepage.dart
@@ -14,12 +14,42 @@ class _UpgradePageState extends State<UpgradePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('업그레이드'),
+        centerTitle: true,
+      ),
       // ignore: avoid_unnecessary_containers
       body: Container(
-        child: const Center(
-          child: Text('Upgrade Page'),
-        ),
-      ),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text('현재 키누적'),
+          const SizedBox(
+            height: 4,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text(
+                'w키 누적 : ${keyCountsSum['w']}',
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                'a키 누적 : ${keyCountsSum['a']}',
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                's키 누적 : ${keyCountsSum['s']}',
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                'd키 누적 : ${keyCountsSum['d']}',
+                textAlign: TextAlign.center,
+              ),
+            ],
+          )
+        ],
+      )),
     );
   }
 }
